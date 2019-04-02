2584dcd88e6ccd4186b7e639a5056d1e3266020a
Romain FENNINGER <romain@fenninger.fr>
2019-04-02 17:16:54 +0200

    Box4b theming

--- app/helpers/server.js
+++ app/helpers/server.js
@@ -5,10 +5,11 @@ import { findAll, getLastCheck } from "./datastore";
 const app = express();
 
 const URL_ROOT  = "/"
-
+const STATIC_PATH = `${__dirname}/../static`
+app.use(express.static(STATIC_PATH))
 app.get(URL_ROOT, (req, res) => {
 
-  let body = `<div className="container" style='display:flex; flex-direction: row; justify-content: flex-start; align-items: center; flex-wrap: wrap; '>`
+  let body = `<div class="container">`
   return getLastCheck()
   .then(lastCheck => {
     return findAll()
@@ -16,8 +17,8 @@ app.get(URL_ROOT, (req, res) => {
       targetList.map(target => {
         const {name, host, alive} = target
         body = `${body}
-        <div style="display:flex; flex-direction: row; padding: 10px; justify-content:flex-start; align-items: center; min-width: 400px; flex-wrap: wrap;">
-          <div style='width: 30px; height:30px; border-radius:50%; background-color:${(alive) ? "#5cb85c": "#b30c0c"}'></div>
+        <div class="item-container">
+          <div class="icon" style='background-color:${(alive) ? "#5cb85c": "#b30c0c"}'></div>
           <div style="padding-left: 10px;">${name} ${(alive) ? "" : "DOWN :("}</div>
         </div>`
       })
@@ -29,17 +30,43 @@ app.get(URL_ROOT, (req, res) => {
 
         body {
           font-family: 'Lato', sans-serif;
+          background-color: #003956;
+          color: white;
         }
 
         button {
           font-family: 'Lato', sans-serif;
-          font-size: 18px;
+          font-size: 16px;
           padding: 5px 9px;
           border-radius: 5px;
           color: #fff;
           background-color: #5bc0de;
           border-color: #46b8da;
           cursor: pointer;
+        }
+
+        .container {
+          display:flex;
+          flex-direction: row;
+          justify-content: flex-start;
+          align-items: center;
+          flex-wrap: wrap;
+        }
+
+        .item-container {
+          display:flex;
+          flex-direction: row;
+          padding: 10px;
+          justify-content:flex-start;
+          align-items: center;
+          min-width: 400px;
+          flex-wrap: wrap;
+        }
+
+        .icon {
+          width: 30px;
+          height:30px;
+          border-radius:50%;
         }
 
       `
@@ -50,6 +77,9 @@ app.get(URL_ROOT, (req, res) => {
           <style type='text/css'>${css}</style>
         </head>
         <body>
+          <div style="">
+            <img src='/logo.png' style="width: 200px;" />
+          <div>
           ${body}
           <div style="padding: 10px;">
             <div style="padding-bottom: 10px;">Last checked on ${lastCheck.toLocaleDateString()}  ${lastCheck.toLocaleTimeString()}</div>
