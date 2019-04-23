0bc3d8367e91835cd7621845f67d564ab210ce3c
Romain FENNINGER <romain@fenninger.fr>
2019-04-23 21:32:38 +0200

    TImeout Ping

--- app/helpers/network.js
+++ app/helpers/network.js
@@ -2,7 +2,7 @@ import {Socket} from "net"
 import { runCommand } from "../libs/System";
 
 export const ping = ({host, timeout = 0.5, pingCount = 3}) => {
-  const command = `ping -i 0.1 -c ${pingCount} -W ${timeout} ${host}`
+  const command = `ping -i 0.1 -t 1 -c ${pingCount} -W ${timeout} ${host}`
   // console.log(command)
   return runCommand(command)
 }
